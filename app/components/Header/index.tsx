27a86c0188ea757e22149e7801fa635f4bd6ff14
Jorge Muñoz Huércano <jbmunoz@protonmail.com>
2025-03-26 19:35:27 +0100

    Added background image, texts, and styles to the Header component

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -6,17 +6,34 @@ import Nav from '../Nav';
 
 export default function Header(): ReactNode {
   return (
-    <header className='flex justify-center px-4 py-8'>
-      <Link href='#'>
-        <Image
-          src={logo}
-          priority
-          alt='Mountains Logo'
-          width={250}
-          height={23}
-        />
-      </Link>
+    <header className='relative flex flex-col justify-between items-center flex-wrap h-screen bg-[url("/img/header_sm.jpg")] bg-cover bg-center py-8'>
       <Nav />
+      <div className='absolute top-0 bottom-0 w-screen h-screen bg-[var(--header-rgba-color)] flex flex-col justify-between items-center py-8 px-4'>
+        <Link href='#'>
+          <Image
+            src={logo}
+            priority
+            alt='Mountains Logo'
+            width={250}
+            height={23}
+          />
+        </Link>
+
+        <div className='text-white'>
+          <h1>
+            Llegamos
+            <span className='text-[var(--primary-color)] font-bold block'>
+              de lo más alto
+            </span>
+          </h1>
+          <p className='[text-shadow:_1px_1px_0px_rgba(0,0,0,0.5)]'>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor,
+            quas? Repellat expedita ex autem, iure aliquam odio a suscipit quas
+            sint quia repudiandae eligendi quibusdam facere ipsam voluptatum
+            labore tempora!
+          </p>
+        </div>
+      </div>
     </header>
   );
 }
