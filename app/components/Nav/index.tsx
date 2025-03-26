63248c9de5e07bdc2aff6481911c1ef35e50333e
Jorge Muñoz Huércano <jbmunoz@protonmail.com>
2025-03-26 20:22:41 +0100

    Change styles names to be more consistance with Tailwind

--- app/components/Nav/index.tsx
+++ app/components/Nav/index.tsx
@@ -3,11 +3,11 @@ import Link from 'next/link';
 
 export default function Nav(): ReactNode {
   return (
-    <nav className='menu-bg-color fixed top-0 left-0 w-full p-1 z-10 shadow-md bg-'>
-      <ul className='flex items-center justify-evenly uppercase font-bold text-white transition'>
+    <nav className='bg-[var(--menu-bg-color)] fixed top-0 left-0 w-full p-1 z-10 shadow-md'>
+      <ul className='flex items-center justify-between uppercase font-bold text-white transition'>
         <li>
           <Link
-            className='py-2 px-4 hover-link-color transition'
+            className='py-2 px-4 transition hover:text-[var(--secondary)] hover:[text-shadow:2px_2px_3px_rgba(0,0,0,0.5)]'
             href='#origin'
           >
             Origen
@@ -15,7 +15,7 @@ export default function Nav(): ReactNode {
         </li>
         <li>
           <Link
-            className='py-2 px-4 hover-link-color transition'
+            className='py-2 px-4 transition hover:text-[var(--secondary)] hover:[text-shadow:2px_2px_3px_rgba(0,0,0,0.5)]'
             href='#cultura'
           >
             Cultura
@@ -23,7 +23,7 @@ export default function Nav(): ReactNode {
         </li>
         <li>
           <Link
-            className='py-2 px-4 hover-link-color transition'
+            className='py-2 px-4 transition hover:text-[var(--secondary)] hover:[text-shadow:2px_2px_3px_rgba(0,0,0,0.5)]'
             href='#sostenible'
           >
             Sostenible
