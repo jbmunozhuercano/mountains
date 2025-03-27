932bc5035cea8c670aa38f7de8ab760ed0640029
Jorge Muñoz Huércano <jbmunoz@protonmail.com>
2025-03-27 19:41:59 +0100

    Added new styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import {ReactNode} from 'react';
 import Header from './components/Header';
+import ContentBlocks from './components/ContentBlocks';
 
 export default function Home(): ReactNode {
   return (
     <>
       <Header />
+      <ContentBlocks />
     </>
   );
 }
