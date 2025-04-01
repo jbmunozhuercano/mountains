f6450956a751084463999a53bc2b3e6b486b026b
Jorge Muñoz Huércano <jbmunoz@protonmail.com>
2025-04-01 19:03:13 +0200

    Added new component ContactBlock

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import {ReactNode} from 'react';
 import Header from './components/Header';
 import ContentBlocks from './components/ContentBlocks';
+import ContactBlock from './components/ContactBlock';
 
 export default function Home(): ReactNode {
   return (
     <>
       <Header />
       <ContentBlocks />
+      <ContactBlock />
     </>
   );
 }
