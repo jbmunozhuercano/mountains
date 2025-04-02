c0981699f18e61c90b605834c836bd8b8cb24cae
Jorge Muñoz Huércano <jbmunoz@protonmail.com>
2025-04-02 20:29:54 +0200

    Added styles to tablet query

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -11,27 +11,32 @@ export default function Header(): JSX.Element {
       className='relative flex flex-col justify-between items-center flex-wrap h-screen bg-[url("/img/header_sm.jpg")] bg-cover bg-center py-8'
     >
       <Nav />
-      <div className='absolute top-0 bottom-0 w-full h-screen bg-header flex flex-col justify-between items-center py-8 px-4'>
+      <div className='absolute top-0 bottom-0 w-full h-screen bg-header flex flex-col justify-between items-center py-8 px-4 md:justify-center lg:pt-2'>
         <Link href='#'>
           <Image
             src={logo}
             priority
             alt='Mountains Logo'
+            className='w-64 md:w-108 absolute top-12 left-1/2 -translate-x-1/2 lg:top-2'
             width={250}
             height={23}
           />
         </Link>
 
-        <div className='text-white'>
-          <h1>
+        <div className='text-white md:flex md:mb-36'>
+          <h1 className='text-3xl md:max-w-sm md:text-right md:text-5xl'>
             Llegamos
             <span className='text-primary font-bold block'>de lo más alto</span>
           </h1>
-          <p className='[text-shadow:var(--text-shadow)]'>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor,
-            quas? Repellat expedita ex autem, iure aliquam odio a suscipit quas
-            sint quia repudiandae eligendi quibusdam facere ipsam voluptatum
-            labore tempora!
+          <p className='[text-shadow:var(--text-shadow)] md:ml-8 md:max-w-sm'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+            vel libero eu ipsum auctor aliquam. Vestibulum ante ipsum primis in
+            faucibus orci luctus et ultrices posuere cubilia curae; Integer
+            blandit ultricies erat, non ullamcorper metus ultricies in. Ut
+            consequat, nisl sed dapibus rhoncus, nulla nulla pharetra erat, nec
+            faucibus erat erat in massa. Suspendisse dapibus quam quis ligula
+            suscipit dictum. Integer ac fermentum nibh. Integer commodo interdum
+            quam, in ullamcorper augue mollis quis.
           </p>
         </div>
       </div>
--- app/components/Nav/index.tsx
+++ app/components/Nav/index.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link';
 
 export default function Nav(): ReactNode {
   return (
-    <nav className='bg-menu fixed top-0 left-0 w-full p-1 z-10 shadow-md'>
-      <ul className='flex items-center justify-between uppercase font-bold text-white transition'>
+    <nav className='bg-menu fixed top-0 left-0 right-0 p-1 z-10 shadow-md lg:left-auto lg:bottom-0'>
+      <ul className='flex items-center justify-between uppercase font-bold text-white transition lg:flex-col lg:h-full md:justify-center lg:items-end lg:gap-24'>
         <li>
           <Link
             className='py-2 px-4 transition hover:text-secondary hover:[text-shadow:var(--text-shadow)]'
