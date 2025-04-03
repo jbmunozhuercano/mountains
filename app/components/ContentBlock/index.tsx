dfb0cefcaa7d0b432d99689c5f8a60293e188a8d
Jorge Muñoz Huércano <jbmunoz@protonmail.com>
2025-04-03 18:23:47 +0200

    Added styles to tablet and desktop  media queries

--- app/components/ContentBlock/index.tsx
+++ app/components/ContentBlock/index.tsx
@@ -18,18 +18,38 @@ export default function ContentBlock({block}: Block): JSX.Element {
     <section
       id={block.id}
       className={
-        block.id === 'culture' ? 'py-12 bg-secondary text-white' : 'py-12'
+        block.id === 'culture'
+          ? 'py-12 lg:py-36 bg-secondary text-white'
+          : 'py-12 lg:py-36'
       }
     >
-      <Image
-        className='mb-8'
-        src={block.imgSrc}
-        width={600}
-        height={400}
-        alt={block.altText}
-      />
-      <h3 className='px-4'>{block.title}</h3>
-      <p className='px-4'>{block.text}</p>
+      <div className='lg:flex lg:justify-center lg:items-center md:max-w-2xl lg:max-w-5xl md:mx-auto'>
+        <div
+          className={
+            block.id === 'culture'
+              ? 'lg:order-2 mb-8 lg:w-2/4 lg:mb-0'
+              : 'mb-8 lg:w-2/4 lg:mb-0'
+          }
+        >
+          <Image
+            className='w-full h-full object-cover'
+            src={block.imgSrc}
+            width={600}
+            height={400}
+            alt={block.altText}
+          />
+        </div>
+        <div
+          className={
+            block.id === 'culture'
+              ? 'lg:w-2/4 lg:px-8 lg:text-right'
+              : 'lg:w-2/4 lg:px-8'
+          }
+        >
+          <h3 className='px-4 text-2xl md:text-4xl'>{block.title}</h3>
+          <p className='px-4 lg:text-lg'>{block.text}</p>
+        </div>
+      </div>
     </section>
   );
 }
