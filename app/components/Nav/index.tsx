0c4c84c67a7c79e9942f566dc8b3b6b7942b639b
Jorge Muñoz Huércano <jbmunoz@protonmail.com>
2025-04-05 19:28:01 +0200

    Made disappear the menu on lg devices

--- app/components/Nav/index.tsx
+++ app/components/Nav/index.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export default function Nav(): ReactNode {
   return (
-    <nav className='bg-menu fixed top-0 left-0 right-0 p-1 z-10 shadow-md lg:left-auto lg:bottom-0'>
+    <nav className='bg-menu fixed top-0 left-0 right-0 p-1 z-10 shadow-md lg:left-auto lg:bottom-0 lg:translate-x-[85%] transition-all lg:hover:translate-x-0'>
       <ul className='flex items-center justify-between uppercase font-bold text-white transition lg:flex-col lg:h-full md:justify-center lg:items-end lg:gap-24'>
         <li>
           <Link
