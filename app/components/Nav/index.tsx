3f1ce549ae3e274c70be4936aa4d282c0a69d8a0
Jorge Muñoz Huércano <jbmunoz@protonmail.com>
2025-04-09 19:46:05 +0200

    Fixed typo

--- app/components/Nav/index.tsx
+++ app/components/Nav/index.tsx
@@ -5,7 +5,7 @@ export default function Nav(): ReactNode {
   return (
     <nav className='bg-menu fixed top-0 left-0 right-0 p-1 z-10 shadow-md lg:left-auto lg:bottom-0 lg:translate-x-[85%] transition-all duration-500 lg:hover:translate-x-0'>
       <span className='uppercase font-bold text-white rotate-90 absolute top-3 left-[-10px] hidden lg:block'>
-        Menu
+        Menú
       </span>
       <ul className='flex items-center justify-between uppercase font-bold text-white transition lg:flex-col lg:h-full md:justify-center lg:items-end lg:gap-24'>
         <li>
